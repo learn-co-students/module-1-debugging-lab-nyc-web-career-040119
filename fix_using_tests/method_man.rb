be8efc8e94524fe94d8d7dec45b858ab89873ce8
OctopiWithHats <sdgranoff@gmail.com>
2019-04-08 10:04:53 -0400

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,15 @@
-require 'pry'
+require "pry"
+require_relative '../fix_using_tests/method_man'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  #binding.pry
+  "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
+  #start_game(player1, player2)
+  #binding.pry
   "#{player1} is better than #{player2}"
 end
+
+play_game("player1", "player2")
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,17 @@
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+    snake = "" #this line creates a new empty string, basically
+    s = "s" #this line turns "s" into something we can multiply and push to snake
+    (1..10).each do |i| #while i is between 1 and 10
+      snake += s #add "s" to snake
+    end
   else
-  string
+    string #return the complete string
   end
+
+  # 10 * "s" + string
+  # else
+  # string
+  # end
+  snake + string #add snake string of "s" to first string
 end
